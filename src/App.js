c91aff1674fb25a342d69c546ce1181334bd7014
jade1001 <jadeagapito10@gmail.com>
2019-11-21 14:06:33 +0800

    main nav bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css'
 import Login from './components/Login'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import MainNavBar from './components/MainNavBar'
 
 function App() {
   return (
     <div className='App'>
-      <Login />
+      <MainNavBar />
     </div>
   )
 }
