79fb7d69cfaf2a508f6dc2f3a1fbbb6dbd4c8e7a
Ariane Mejia <ariane@quantumcrowd.io>
2019-11-21 14:40:17 +0800

    added OpsNotif1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
-import Login from './components/Login'
+// import Login from './components/Login'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import OpsNotif1 from './components/OpsNotif1'
 
 function App() {
   return (
     <div className='App'>
-      <Login />
+      {/* <Login /> */}
+      <OpsNotif1 />
     </div>
   )
 }
