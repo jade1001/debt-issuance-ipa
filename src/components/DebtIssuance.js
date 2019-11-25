2fa862c665658cff89c2e88a2fee313d878f8f60
jade1001 <jadeagapito10@gmail.com>
2019-11-25 11:05:30 +0800

    fixed debtissuance responsive

--- src/components/DebtIssuance.js
+++ src/components/DebtIssuance.js
@@ -32,184 +32,123 @@ export class DebtIssuance extends Component {
     })
     return (
       <div>
-        <Container>
+        <Container id='headerId'>
           <Row style={{ marginTop: 20 }}>
             <Col xs>
-              <p
-                style={{
-                  color: '#182e58',
-                  borderBottom: '2px solid #182e58',
-                  width: 130
-                }}
-              >
-                Commercial Paper
-              </p>
+              <p id='pHeader'>Commercial Paper</p>
               <hr style={{ marginTop: -17 }} />
             </Col>
           </Row>
         </Container>
-        <Container
-          style={{
-            width: '15%',
-            height: 1070,
-            float: 'left',
-            paddingLeft: 0,
-            borderRight: '1px solid #E5E5E5'
-          }}
-        >
-          <Col style={{ color: '#182e58', fontSize: 14, cursor: 'pointer' }}>
+        <Container id='sideBar'>
+          <Col id='fMenu'>
             Maturities (ISIN) &nbsp;
             <FontAwesomeIcon icon={faChevronRight} />
           </Col>
-          <Col
-            style={{
-              opacity: 0.4,
-              marginTop: 7,
-              fontSize: 14,
-              cursor: 'pointer'
-            }}
-          >
+          <Col id='dMenu'>
             Tranches &nbsp;
             <FontAwesomeIcon icon={faChevronRight} />
           </Col>
-          <Col
-            style={{
-              opacity: 0.4,
-              marginTop: 7,
-              fontSize: 14,
-              cursor: 'pointer'
-            }}
-          >
+          <Col id='dMenu'>
             Allocations &nbsp;
             <FontAwesomeIcon icon={faChevronRight} />
           </Col>
-          <Col
-            style={{
-              opacity: 0.4,
-              marginTop: 7,
-              fontSize: 14,
-              cursor: 'pointer'
-            }}
-          >
-            + Start New
-          </Col>
+          <Col id='dMenu'>+ Start New</Col>
         </Container>
-        <Container className='App container'>
+        <Container className='App container' id='contFunctions'>
           <Row>
-            <Col sm={4} style={{ marginLeft: 35, marginTop: 10 }}>
-              <FontAwesomeIcon
-                icon={faDownload}
-                style={{
-                  cursor: 'pointer',
-                  width: 50,
-                  height: 25,
-                  color: 'white',
-                  border: '1px solid #182e58',
-                  backgroundColor: '#182e58',
-                  borderRadius: 5
-                }}
-              />
-            </Col>
-            <Col
-              sm={4}
-              align='right'
-              style={{
-                display: 'flex',
-                float: 'right',
-                marginLeft: '23%'
-              }}
-            >
-              <Form inline>
+            <Form inline id='formF'>
+              <Col sm={4} style={{ marginLeft: 35, marginTop: 10 }}>
+                <FontAwesomeIcon icon={faDownload} id='iconDL' />
+              </Col>
+              <Col id='colSearch' sm={4} align='right'>
                 <FormControl
+                  id='searchBox'
                   type='text'
                   className=' mr-sm-2'
-                  style={{ width: 220, height: 30 }}
                   onChange={this.updateSearch.bind(this)}
                 />
                 <Button
+                  id='btnSearch'
                   variant='secondary'
                   size='sm'
                   type='submit'
-                  style={{ height: 30, width: 30, backgroundColor: '#182e58' }}
                 >
-                  <FontAwesomeIcon icon={faSearch} />
+                  <FontAwesomeIcon icon={faSearch} id='iconSearch' />
                 </Button>
-              </Form>
-            </Col>
+              </Col>
+            </Form>
           </Row>
-        </Container>
 
-        <table
-          responsive
-          className='table table-hover table-content'
-          style={{
-            fontSize: 11,
-            width: '82%'
-          }}
-        >
-          <thead>
-            <tr style={{ lineHeight: '100%' }}>
-              <th scope='col' style={{ fontWeight: 'bold' }}>
-                Name
-              </th>
-              <th scope='col' style={{ fontWeight: 'bold' }}>
-                ISIN
-              </th>
-              <th scope='col' style={{ fontWeight: 'bold' }}>
-                Status
-              </th>
-              <th scope='col' style={{ fontWeight: 'bold' }}>
-                Total Redemption Value
-              </th>
-              <th scope='col' style={{ fontWeight: 'bold' }}>
-                Tenor
-              </th>
-              <th scope='col' style={{ fontWeight: 'bold' }}>
-                Maturity Date
-              </th>
-              <th scope='col' style={{ fontWeight: 'bold' }}>
-                Created
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {filtered.map((n, i) => (
-              <tr
-                key={i}
-                data-toggle='modal'
-                data-target='#myModal'
-                id='tRow'
-                style={{
-                  cursor: 'pointer',
-                  color: '#182E58',
-                  lineHeight: '100%'
-                }}
-              >
-                <td style={{ border: 'none' }}>
-                  <span>{n.name}</span>
-                </td>
-                <td style={{ border: 'none' }}>
-                  <span>{n.isin}</span>
-                </td>
-                <td style={{ border: 'none' }}>
-                  <span>{n.status}</span>
-                </td>
-                <td style={{ border: 'none' }}>
-                  <span>{n.trvalue}</span>
-                </td>
-                <td style={{ border: 'none' }}>
-                  <span>{n.tenor}</span>
-                </td>
-                <td style={{ border: 'none' }}>
-                  <span>{n.mdate}</span>
-                </td>
-                <td style={{ border: 'none' }}>
-                  <span>{n.created}</span>
-                </td>
+          <table
+            id='tableId'
+            responsive
+            className='table table-hover table-content'
+          >
+            <thead>
+              <tr style={{ lineHeight: '100%' }}>
+                <th scope='col' style={{ fontWeight: 'bold' }}>
+                  Name
+                </th>
+                <th scope='col' style={{ fontWeight: 'bold' }}>
+                  ISIN
+                </th>
+                <th scope='col' style={{ fontWeight: 'bold' }}>
+                  Status
+                </th>
+                <th scope='col' style={{ fontWeight: 'bold' }}>
+                  Total Redemption Value
+                </th>
+                <th scope='col' style={{ fontWeight: 'bold' }}>
+                  Tenor
+                </th>
+                <th scope='col' style={{ fontWeight: 'bold' }}>
+                  Maturity Date
+                </th>
+                <th scope='col' style={{ fontWeight: 'bold' }}>
+                  Created
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filtered.map((n, i) => (
+                <tr
+                  key={i}
+                  data-toggle='modal'
+                  data-target='#myModal'
+                  id='tRow'
+                  style={{
+                    cursor: 'pointer',
+                    color: '#182E58',
+                    lineHeight: '100%'
+                  }}
+                >
+                  <td style={{ border: 'none' }}>
+                    <span>{n.name}</span>
+                  </td>
+                  <td style={{ border: 'none' }}>
+                    <span>{n.isin}</span>
+                  </td>
+                  <td style={{ border: 'none' }}>
+                    <span>{n.status}</span>
+                  </td>
+                  <td style={{ border: 'none' }}>
+                    <span>{n.trvalue}</span>
+                  </td>
+                  <td style={{ border: 'none' }}>
+                    <span>{n.tenor}</span>
+                  </td>
+                  <td style={{ border: 'none' }}>
+                    <span>{n.mdate}</span>
+                  </td>
+                  <td style={{ border: 'none' }}>
+                    <span>{n.created}</span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Container>
       </div>
     )
   }
