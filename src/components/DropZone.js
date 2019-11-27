6761e0f2f99e492fe3e61c8401c972121aec1c13
jade1001 <jadeagapito10@gmail.com>
2019-11-27 15:53:13 +0800

    added dropzone file display

--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -1,7 +1,8 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import styled from 'styled-components'
-import { Button, FormLabel } from 'react-bootstrap'
+import { Button, FormLabel, Col, Row, Container } from 'react-bootstrap'
+import csvFile from './csvfile.png'
 
 const getColor = props => {
   if (props.isDragAccept) {
@@ -16,7 +17,7 @@ const getColor = props => {
   return '#919191'
 }
 
-const Container = styled.div`
+const Cont = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
@@ -35,9 +36,23 @@ const Container = styled.div`
 `
 
 function DropZone() {
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
+  const [listOfFiles, setListOfFiles] = useState({ files: [] })
+  const [show, setShow] = useState('hidden')
+  const [data, showData] = useState('')
+  const onDrop = useCallback(files => {
+    setListOfFiles({ files })
+    setShow('')
+    showData('hidden')
+    console.log(
+      files
+        .map(f => {
+          return f.name
+        })
+        .join(''),
+      { files }
+    )
   }, [])
+
   const {
     getRootProps,
     getInputProps,
@@ -49,6 +64,7 @@ function DropZone() {
     accept:
       'application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
   })
+
   return (
     <div>
       <div
@@ -70,18 +86,56 @@ function DropZone() {
           BENPOS Report
         </FormLabel>
       </div>
-      <Container
-        {...getRootProps({ isDragActive, isDragAccept, isDragReject })}
-      >
+      <Cont {...getRootProps({ isDragActive, isDragAccept, isDragReject })}>
         <input {...getInputProps()} />
-        <p>Drag and drop .CSV file here or click</p>
+        <p style={{ visibility: data }}>
+          Drag and drop .CSV file here or click
+        </p>
         <Button
           size='sm'
-          style={{ backgroundColor: '#33466B', border: 'none' }}
+          style={{
+            backgroundColor: '#33466B',
+            border: 'none',
+            visibility: data
+          }}
         >
           Upload
         </Button>
-      </Container>
+        <div>
+          {listOfFiles.files.map(n => (
+            <Container>
+              <Col style={{ marginTop: -60 }}>
+                <Row
+                  key={n.name}
+                  style={{
+                    margin: 'auto',
+                    width: 50
+                  }}
+                >
+                  <img src={csvFile} style={{ width: 50, height: 50 }} />
+                </Row>
+                <Row>
+                  <a href='#' style={{ color: 'black' }}>
+                    {n.name}
+                  </a>
+                </Row>
+              </Col>
+            </Container>
+          ))}
+        </div>
+      </Cont>
+      <div align='right' style={{ width: '90%', marginTop: 10 }}>
+        <Button
+          size='sm'
+          style={{
+            backgroundColor: '#33466B',
+            border: 'none',
+            visibility: show
+          }}
+        >
+          Submit
+        </Button>
+      </div>
     </div>
   )
 }
