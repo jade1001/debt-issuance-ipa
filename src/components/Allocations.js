96b013c62ddf0879bf6ca924efd1f18ed9181023
cyrill <cyrill@quantumcrowd.io>
2019-11-29 11:04:29 +0800

    Added Redemption2

--- src/components/Allocations.js
+++ src/components/Allocations.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-import { Table, Container, Dropdown, Accordion, Button } from "react-bootstrap"
-import "./accordion.css"
+import React, { Component } from 'react'
+import { Table, Container, Dropdown, Accordion, Button } from 'react-bootstrap'
+import './accordion.css'
 
 export class Allocations extends Component {
   render() {
@@ -20,12 +20,13 @@ export class Allocations extends Component {
       <Container>
         <Table
           style={{
-            width: "70%",
+            width: '70%',
             marginTop: 15,
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginBottom: "auto"
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            marginBottom: 'auto'
           }}
+          className='Modal-Allocation-Table'
         >
           <thead>
             <tr>
@@ -51,7 +52,7 @@ export class Allocations extends Component {
               <td>08/07/2019</td>
               <td>09/07/2019</td>
               <td>50,00,00.000 INR</td>
-              <td style={{ textAlign: "right" }}>9.58</td>
+              <td style={{ textAlign: 'right' }}>9.58</td>
               <td>
                 <Dropdown drop='left'>
                   <Dropdown.Toggle
@@ -60,7 +61,7 @@ export class Allocations extends Component {
                   >
                     <i className='fa fa-ellipsis-v' aria-hidden='true'></i>
                   </Dropdown.Toggle>
-                  <Dropdown.Menu style={{ fontSize: "0.6rem" }}>
+                  <Dropdown.Menu style={{ fontSize: '0.6rem' }}>
                     <Dropdown.Item eventKey='1' disabled>
                       Action
                     </Dropdown.Item>
@@ -71,17 +72,17 @@ export class Allocations extends Component {
               </td>
             </tr>
             <tr>
-              <td id='tdalloc' style={{ border: "none" }}>
+              <td id='tdalloc' style={{ border: 'none' }}>
                 <i class='fa fa-angle-right' id='angle'></i>
               </td>
-              <td style={{ border: "none" }}>Mutual Fund of India</td>
-              <td style={{ border: "none" }}></td>
-              <td style={{ border: "none" }}>Monetago India Pvt Ltd</td>
-              <td style={{ border: "none" }}>08/07/2019</td>
-              <td style={{ border: "none" }}>09/07/2019</td>
-              <td style={{ border: "none" }}>50,00,00.000 INR</td>
-              <td style={{ border: "none", textAlign: "right" }}>9.58</td>
-              <td style={{ border: "none" }}>
+              <td style={{ border: 'none' }}>Mutual Fund of India</td>
+              <td style={{ border: 'none' }}></td>
+              <td style={{ border: 'none' }}>Monetago India Pvt Ltd</td>
+              <td style={{ border: 'none' }}>08/07/2019</td>
+              <td style={{ border: 'none' }}>09/07/2019</td>
+              <td style={{ border: 'none' }}>50,00,00.000 INR</td>
+              <td style={{ border: 'none', textAlign: 'right' }}>9.58</td>
+              <td style={{ border: 'none' }}>
                 <Dropdown drop='left'>
                   <Dropdown.Toggle
                     as={CustomToggle}
@@ -89,7 +90,7 @@ export class Allocations extends Component {
                   >
                     <i className='fa fa-ellipsis-v' aria-hidden='true'></i>
                   </Dropdown.Toggle>
-                  <Dropdown.Menu style={{ fontSize: "0.6rem" }}>
+                  <Dropdown.Menu style={{ fontSize: '0.6rem' }}>
                     <Dropdown.Item eventKey='1' disabled>
                       Action
                     </Dropdown.Item>
