8e930dcd4d6fdb47b022d343d1891bfdb0085a81
Ronald Antonio <ronald.antonio@mcveanpacific.com>
2019-12-02 13:38:30 +0800

    Login, LoginNavbar UI update

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,8 @@ import {
   FormLabel,
   InputGroup,
   Toast,
-  Spinner
+  Spinner,
+  Container
 } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faKey } from '@fortawesome/free-solid-svg-icons'
@@ -18,207 +19,165 @@ import { Redirect } from 'react-router-dom'
 import { history } from '../Redux/_helpers/history'
 
 class Login extends Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-    // this.props.logout()
-    this.state = {
-      Email: '',
-      Password: '',
-      submitted: false,
-      visibility: 'hidden',
-      setShow: true,
-      spinner: '',
-      color: ''
-    }
+		// this.props.logout()
+		this.state = {
+		Email: '',
+		Password: '',
+		submitted: false,
+		visibility: 'hidden',
+		setShow: true,
+		spinner: '',
+		color: ''
+		}
 
-    this.emailOnChangeHandler = this.emailOnChangeHandler.bind(this)
-    this.passOnChangeHandler = this.passOnChangeHandler.bind(this)
-    this.change = this.change.bind(this)
-    this.Login = this.Login.bind(this)
-  }
+		this.emailOnChangeHandler = this.emailOnChangeHandler.bind(this)
+		this.passOnChangeHandler = this.passOnChangeHandler.bind(this)
+		this.change = this.change.bind(this)
+		this.Login = this.Login.bind(this)
+	}
 
-  change() {
-    if (this.email.value.length > 0 && this.pass.value.length > 0) {
-      this.setState({ color: '#192f59' })
-    } else if (this.pass.value.length === 0 && this.pass.value.length === 0) {
-      this.setState({ color: '' })
-    }
-  }
+	change() {
+		if (this.email.value.length > 0 && this.pass.value.length > 0) {
+		this.setState({ color: '#192f59' })
+		} else if (this.pass.value.length === 0 && this.pass.value.length === 0) {
+		this.setState({ color: '' })
+		}
+	}
 
-  emailOnChangeHandler(event) {
-    this.setState({
-      ...this.state,
-      Email: event.target.value
-    })
-  }
+	emailOnChangeHandler(event) {
+		this.setState({
+		...this.state,
+		Email: event.target.value
+		})
+	}
 
-  passOnChangeHandler(event) {
-    this.setState({
-      ...this.state,
-      Password: event.target.value
-    })
-  }
+	passOnChangeHandler(event) {
+		this.setState({
+		...this.state,
+		Password: event.target.value
+		})
+	}
 
-  Login(e) {
-    e.preventDefault()
-    this.setState({ spinner: 'border' })
-    if (
-      this.state.Email === 'ipa.ops@ipabank.com' &&
-      this.state.Password === 'password'
-    ) {
-      this.setState({ submitted: true })
-      setTimeout(
-        function() {
-          this.setState({ spinner: '' })
-          const { Email, Password } = this.state
-          if (Email && Password) {
-            this.props.login(Email, Password)
-          }
-        }.bind(this),
-        2000
-      )
-    } else {
-      this.setState({ visibility: '' })
-      setTimeout(
-        function() {
-          this.setState({ spinner: '' })
-        }.bind(this),
-        500
-      )
-      setTimeout(
-        function() {
-          this.setState({ visibility: 'hidden' })
-        }.bind(this),
-        3000
-      )
-    }
-  }
+	Login(e) {
+		e.preventDefault()
+		this.setState({ spinner: 'border' })
+		if (
+		this.state.Email === 'ipa.ops@ipabank.com' &&
+		this.state.Password === 'password'
+		) {
+		this.setState({ submitted: true })
+		setTimeout(
+			function() {
+			this.setState({ spinner: '' })
+			const { Email, Password } = this.state
+			if (Email && Password) {
+				this.props.login(Email, Password)
+			}
+			}.bind(this),
+			2000
+		)
+		} else {
+		this.setState({ visibility: '' })
+		setTimeout(
+			function() {
+			this.setState({ spinner: '' })
+			}.bind(this),
+			500
+		)
+		setTimeout(
+			function() {
+			this.setState({ visibility: 'hidden' })
+			}.bind(this),
+			3000
+		)
+		}
+	}
 
-  getUser() {}
+	getUser() {}
 
-  render() {
-    return (
-      <div>
-        <LoginNavBar />
-        <Toast
-          delay={3000}
-          autohide
-          onClose={() => this.setState({ visibility: 'hidden' })}
-          style={{ visibility: this.state.visibility, margin: 'auto' }}
-        >
-          <Toast.Header>
-            <strong className='mr-auto' style={{ color: 'red' }}>
-              Opps!
-            </strong>
-          </Toast.Header>
-          <Toast.Body style={{ color: 'red' }}>
-            Your email or password is incorrect!
-          </Toast.Body>
-        </Toast>
-        <div style={{ textAlign: 'center' }}>
-          <FormLabel
-            style={{
-              fontSize: 30,
-              marginTop: '8% ',
-              color: '#182e58',
-              backgroundColor: ''
-            }}
-          >
-            Treasury Debt Issuance
-          </FormLabel>{' '}
-          <br />
-          <FormLabel style={{ color: '#182e58', marginTop: 20 }}>
-            Login To Your Account
-          </FormLabel>
-        </div>
-        <Form
-          style={{
-            width: 300,
-            margin: 'auto',
-            marginTop: 10
-          }}
-          onSubmit={this.Login}
-          onChange={this.change}
-        >
-          <Form.Group controlId='formBasicEmail'>
-            <InputGroup.Prepend>
-              <InputGroup.Text id='basic-addon1' style={{ borderRadius: 0 }}>
-                <FontAwesomeIcon
-                  icon={faIdCardAlt}
-                  style={{ color: '#919191' }}
-                />
-              </InputGroup.Text>
-              <Form.Control
-                required
-                type='email'
-                placeholder='User ID / Email'
-                style={{
-                  width: '300px',
-                  color: '#182e58',
-                  borderRadius: '0 0 0 0'
-                }}
-                onChange={this.emailOnChangeHandler}
-                ref={email => (this.email = email)}
-              />
-            </InputGroup.Prepend>
-          </Form.Group>
+	render() {
+		return (
+			<>
+				<LoginNavBar />
+				<Container>
+					<Toast delay={3000} autohide onClose={() => this.setState({ visibility: 'hidden' })} style={{ visibility: this.state.visibility, margin: 'auto' }}>
+						<Toast.Header>
+							<strong className='mr-auto' style={{ color: 'red' }}>
+								Opps!
+							</strong>
+						</Toast.Header>
+						<Toast.Body style={{ color: 'red' }}>
+							Your email or password is incorrect!
+						</Toast.Body>
+					</Toast>
+					<div style={{ textAlign: 'center' }}>
+						<FormLabel style={{fontSize: 30,marginTop: '8% ',color: '#182e58'}}>
+							Treasury Debt Issuance
+						</FormLabel>
+						<br />
+						<FormLabel style={{ color: '#182e58', marginTop: 20 }}>
+							Login To Your Account
+						</FormLabel>
+					</div>
+					<Form onSubmit={this.Login} onChange={this.change} style={{width: 300,margin: 'auto', marginTop: 10}}>
+						<Form.Group controlId='formBasicEmail'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1' style={{ borderRadius: 0 }}>
+									<FontAwesomeIcon icon={faIdCardAlt} style={{ color: '#919191' }}/>
+								</InputGroup.Text>
+								<Form.Control
+									required
+									type='email'
+									placeholder='User ID / Email'
+									style={{width: '300px',color: '#182e58',borderRadius: '0px'}}
+									onChange={this.emailOnChangeHandler}
+									ref={email => (this.email = email)}/>
+							</InputGroup.Prepend>
+						</Form.Group>
 
-          <Form.Group controlId='formBasicPassword'>
-            <InputGroup.Prepend>
-              <InputGroup.Text
-                id='basic-addon1'
-                style={{ borderRadius: 0, paddingRight: 14 }}
-              >
-                <FontAwesomeIcon icon={faKey} style={{ color: '#919191' }} />
-              </InputGroup.Text>
-              <Form.Control
-                required
-                type='password'
-                placeholder='Password'
-                style={{
-                  width: '300px',
-                  color: '#182e58',
-                  borderRadius: '0 0 0 0'
-                }}
-                onChange={this.passOnChangeHandler}
-                ref={pass => (this.pass = pass)}
-              />
-            </InputGroup.Prepend>
-          </Form.Group>
-          <Form.Group controlId='formBasicCheckbox'>
-            <Nav.Link
-              href='#forgot'
-              style={{
-                fontSize: 13,
-                marginLeft: -15,
-                float: 'left',
-                marginTop: -12
-              }}
-            >
-              Forgot Password?
-            </Nav.Link>
-          </Form.Group>
-          <Button
-            variant='secondary'
-            size='sm'
-            type='submit'
-            style={{
-              fontSize: 13,
-              width: 82,
-              height: 30,
-              marginLeft: 98,
-              backgroundColor: this.state.color,
-              border: 'none'
-            }}
-          >
-            <Spinner animation={this.state.spinner} size='sm' />
-            &nbsp; Sign In
-          </Button>
-        </Form>
-      </div>
-    )
-  }
+						<Form.Group controlId='formBasicPassword'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1' style={{ borderRadius: 0, paddingRight: 14 }}>
+									<FontAwesomeIcon icon={faKey} style={{ color: '#919191' }} />
+								</InputGroup.Text>
+								<Form.Control
+									required
+									type='password'
+									placeholder='Password'
+									style={{width: '300px',color: '#182e58',borderRadius: '0px'}}
+									onChange={this.passOnChangeHandler}
+									ref={pass => (this.pass = pass)}/>
+							</InputGroup.Prepend>
+						</Form.Group>
+						<Form.Group controlId='formBasicCheckbox'>
+							<Nav.Link href='#forgot' style={{fontSize: 13, marginLeft: -15, float: 'left', marginTop: -12}}>
+								Forgot Password?
+							</Nav.Link>
+						</Form.Group>
+						<Button
+							variant='secondary'
+							size='sm'
+							type='submit'
+							style={{
+							fontSize: 13,
+							width: 82,
+							height: 30,
+							marginLeft: 98,
+							backgroundColor: this.state.color,
+							border: 'none'
+							}}
+						>
+							<Spinner animation={this.state.spinner} size='sm' />
+							&nbsp; Sign In
+						</Button>
+					</Form>
+				</Container>
+			</>
+		)
+	}
 }
 
 function mapState(state) {
--- src/components/LoginNavBar.js
+++ src/components/LoginNavBar.js
@@ -1,43 +1,31 @@
 import React, { Component } from 'react'
 import '../App.css'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import logo from './logo.png'
 
 class LoginNavBar extends Component {
-  render() {
-    return (
-      <div className='App container'>
-        <Navbar
-          fluid='true'
-          collapseOnSelect
-          expand='lg'
-          style={{ backgroundColor: 'rgb(245, 245, 245)', fontSize: '14px' }}
-          variant='light'
-        >
-          <Navbar.Brand href='#home'>
-            <img
-              alt=''
-              src={logo}
-              width='30'
-              height='30'
-              className='d-inline-block align-top'
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Nav.Link href='#signin' className='nav'>
-                Sign In
-              </Nav.Link>
-              <Nav.Link href='#help' className='nav'>
-                Help
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<Navbar fluid='true' collapseOnSelect expand='lg' variant='light' style={{ backgroundColor: 'rgb(245, 245, 245)', fontSize: '14px' }}>
+				<Container>
+					<Navbar.Brand href='#home'>
+						<img src={logo} alt='' width='30' height='30' className='d-inline-block align-top'/>
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav className='ml-auto'>
+							<Nav.Link href='#signin' className='nav'>
+								Sign In
+							</Nav.Link>
+							<Nav.Link href='#help' className='nav'>
+								Help
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		)
+	}
 }
 
 export default LoginNavBar
